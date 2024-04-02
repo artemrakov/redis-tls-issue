ca83149f1b741a435c58a1ab8a36e5a05a4bb686
Artem Rakov <arakov@amazon.com>
2024-04-02 19:35:21 -0400

    async

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,40 @@
-use std::io::Read;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use std::{fs::File, io::BufReader};
 
-use redis::{Client, ClientTlsConfig, Commands, TlsCertificates};
+use redis::Client;
 
-#[derive(Clone)]
-pub struct RedisClient {
-    pool: r2d2::Pool<Client>,
-}
-
-impl RedisClient {
-    pub fn new(url: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let client = Client::open(url).unwrap();
-        let pool = r2d2::Pool::builder()
-            .max_size(10)
-            .min_idle(Some(5))
-            .build(client)
-            .unwrap();
-
-        let redis = RedisClient { pool };
-        Ok(redis)
-    }
-
-    pub fn get(&self, key: &str) -> Result<Option<Vec<u8>>, redis::RedisError> {
-        let mut con = self.pool.get().unwrap();
-        let value: Vec<u8> = redis::cmd("GET").arg(key).query(&mut con).unwrap();
-
-        Ok(Some(value))
-    }
-
-    pub fn set(&self, key: &str, value: Vec<u8>) -> Result<(), redis::RedisError> {
-        let mut con = self.pool.get().unwrap();
-
-        redis::cmd("SET").arg(key).arg(value).query(&mut con)?;
-
-        Ok(())
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Connecting to redis");
-    let client = RedisClient::new("rediss://127.0.0.1:6379#insecure").unwrap();
+    let client = Client::open("rediss://127.0.0.1:6379#insecure").unwrap();
 
     println!("Connected to redis. Starting the loop");
-
     let value = vec![0; 20 * 1024 * 1024]; // 20MB value
-    client.set("key", value).unwrap();
+
+    let mut con = client.get_multiplexed_tokio_connection().await.unwrap();
+    let _: Result<(), redis::RedisError> = redis::cmd("SET")
+        .arg("key")
+        .arg(value)
+        .query_async(&mut con)
+        .await;
 
     for i in 1..10 {
-        println!("This is loop number {}", i);
+        let mut con = con.clone();
         let start_time = Instant::now();
 
-        let result = client.get("key").unwrap();
-        println!("Is some: {}", result.is_some());
+        let value: Option<Vec<u8>> = redis::cmd("GET")
+            .arg("key")
+            .query_async(&mut con)
+            .await
+            .unwrap();
 
-        let end_time = start_time.elapsed().as_millis();
-        println!("Redis get latency_ms={}", end_time);
+        let duration = start_time.elapsed().as_millis();
+        println!(
+            "LoopNumber: {}, RedisGet latency_ms={}, value.is_some()={}",
+            i,
+            duration,
+            value.is_some()
+        );
 
         sleep(Duration::from_secs(1))
     }
