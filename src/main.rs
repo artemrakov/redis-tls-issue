f72c8541c69bafc810e1db3492ff6bed7446c4d0
Artem Rakov <arakov@amazon.com>
2024-04-02 20:12:22 -0400

    using bb8

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
+use bb8_redis::{bb8, RedisConnectionManager};
 use redis::Client;
 
 #[tokio::main]
 async fn main() {
     println!("Connecting to redis");
-    let client = Client::open("rediss://127.0.0.1:6379#insecure").unwrap();
+    // let client = Client::open("rediss://127.0.0.1:6379#insecure").unwrap();
+
+    let manager = RedisConnectionManager::new("rediss://127.0.0.1:6379#insecure").unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
 
     println!("Connected to redis. Starting the loop");
     let value = vec![0; 20 * 1024 * 1024]; // 20MB value
 
-    let mut con = client.get_multiplexed_tokio_connection().await.unwrap();
+    // let mut con = client.get_multiplexed_tokio_connection().await.unwrap();
+    let mut con = pool.get().await.unwrap();
     let _: Result<(), redis::RedisError> = redis::cmd("SET")
         .arg("key")
         .arg(value)
-        .query_async(&mut con)
+        .query_async(&mut *con)
         .await;
 
     for i in 1..10 {
-        let mut con = con.clone();
+        let mut con = pool.get().await.unwrap();
         let start_time = Instant::now();
 
         let value: Option<Vec<u8>> = redis::cmd("GET")
             .arg("key")
-            .query_async(&mut con)
+            .query_async(&mut *con)
             .await
             .unwrap();
 
